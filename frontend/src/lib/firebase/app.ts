3c2f5a14eab0adb29341cca3fbd214bbf2a26bc8
Daniel Ebert <pancomputationalist@mailbox.org>
2022-06-24 21:05:19 +0000

    add firebase

---
+++ frontend/src/lib/firebase/app.ts
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
+export const app = initializeApp({
+  apiKey: 'AIzaSyADNedteT7Qy8wjBPB9n0Xvq0vzNLG9txc',
+  authDomain: 'pothos-dev.firebaseapp.com',
+  projectId: 'pothos-dev',
+  storageBucket: 'pothos-dev.appspot.com',
+  messagingSenderId: '566111881551',
+  appId: '1:566111881551:web:252afdde67582654161d3a',
+})
