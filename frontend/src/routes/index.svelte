f2660f945ec9aa1e3569588534921f2b687fee24
Daniel Ebert <pancomputationalist@mailbox.org>
2022-06-24 20:38:23 +0000

    formatting

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import AuthenticationButton from 'src/features/Authentication/components/AuthenticationButton.svelte'
+</script>
+
+<AuthenticationButton />
