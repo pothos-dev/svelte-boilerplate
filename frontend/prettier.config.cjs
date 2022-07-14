9bb7ce148a44be3a975cfe198dc4038837129bf9
Daniel Ebert <pancomputationalist@mailbox.org>
2022-07-14 16:33:43 +0000

    finally got autoformatting to work

---
+++ frontend/prettier.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  "useTabs": false,
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "semi": false,
+  "arrowParens": "avoid",
+  "printWidth": 100,
+  
+  "svelteStrictMode": true,
+  "svelteIndentScriptAndStyle": false,
+
+  "tailwindConfig": "./tailwind.config.cjs",
+  "plugins": [require("prettier-plugin-tailwindcss")],
+  "overrides": [
+    {
+      "files": "*.svelte",
+      "options": { "parser": "svelte" }
+    }
+  ]
+}
