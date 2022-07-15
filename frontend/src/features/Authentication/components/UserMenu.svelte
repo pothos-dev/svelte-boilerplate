e12b8b5f80b68694de2d7b9c88279bbec87c39db
Daniel Ebert <pancomputationalist@mailbox.org>
2022-07-15 11:51:40 +0000

    fix menu styling

--- frontend/src/features/Authentication/components/UserMenu.svelte
+++ frontend/src/features/Authentication/components/UserMenu.svelte
@@ -1,32 +1,35 @@
 <script lang="ts">
 import { currentUser } from '~/features/Authentication/states'
+import { auth } from '~/lib/firebase/auth'
+
+async function logout() {
+  await auth.signOut()
+}
 </script>
 
 {#if $currentUser}
   {@const { photoURL, displayName } = $currentUser}
 
   <!-- Menu Container -->
-  <div class="dropdown-end dropdown dropdown-open">
+  <div class="dropdown-end dropdown">
     <!-- Avatar Icon -->
     <img class="h-10 w-10 rounded-full" tabindex="0" src="{photoURL}" alt="{displayName}" />
 
     <!-- Menu Content -->
-    <ul tabindex="0" class="dropdown-content menu gap-2 rounded-md bg-base-100 p-2 shadow">
-      <!-- <li>Profile</li> -->
+    <ul tabindex="0" class="dropdown-content menu rounded-md bg-base-100 p-2 shadow">
+      <li>
+        <div>
+          <span class="iconify" data-icon="material-symbols:person-outline"></span>
+          Profile
+        </div>
+      </li>
 
-      <li class="light-blue-block">
-        <!-- <button class="btn gap-2"> -->
+      <li>
+        <div on:click="{logout}">
           <span class="iconify" data-icon="material-symbols:logout"></span>
           Logout
-        <!-- </button> -->
+        </div>
       </li>
     </ul>
   </div>
 {/if}
-
-
-<style>
-  .light-blue-block .iconify {
-   color: #08f;
-}
-</style>
