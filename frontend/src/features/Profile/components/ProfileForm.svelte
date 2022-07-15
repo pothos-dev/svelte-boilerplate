941ef8e393c6ec3345dd7e9ceb5fd75b7f911792
Daniel Ebert <pancomputationalist@mailbox.org>
2022-07-15 12:29:16 +0000

    ProfileForm

---
+++ frontend/src/features/Profile/components/ProfileForm.svelte
@@ -0,0 +1,11 @@
+<div class="form-control w-full max-w-md gap-2">
+  <label class="input-group">
+    <span>@</span>
+    <input class="input input-bordered w-full" type="text" placeholder="username" />
+  </label>
+
+  <input class="input input-bordered w-full" type="text" placeholder="First name" />
+  <input class="input input-bordered w-full" type="text" placeholder="Last name" />
+
+  <input class="btn" type="submit" value="Save profile" />
+</div>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,1 +1,5 @@
-Hello world
+<script>
+import ProfileForm from '~/features/Profile/components/ProfileForm.svelte'
+</script>
+
+<ProfileForm />
